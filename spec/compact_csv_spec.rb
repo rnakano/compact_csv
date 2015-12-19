850a0bdda13bb516c3ba90d8f80e8316acad31a9
NAKANO Ryusuke <rsk.nakano@gmail.com>
2015-12-19 15:42:17 +0900

    add spec

--- spec/compact_csv_spec.rb
+++ spec/compact_csv_spec.rb
@@ -5,9 +5,30 @@ describe CompactCSV do
     expect(CompactCSV::VERSION).not_to be nil
   end
 
-  it 'loads csv without error' do
-    csv_path = './spec/data/sample.csv'
-    csv = CompactCSV.read(csv_path, headers: true)
-    expect(csv).to be_truthy
+  describe '.read' do
+    let(:csv_path) { './spec/data/sample.csv' }
+
+    it 'loads Array instance from csv file' do
+      csv = CompactCSV.read(csv_path)
+      expect(csv).to be_instance_of(Array)
+    end
+
+    it 'loads CompactCSV::Table instance from csv file' do
+      csv = CompactCSV.read(csv_path, headers: true)
+      expect(csv).to be_instance_of(CompactCSV::Table)
+      expect(csv).to be_kind_of(CSV::Table)
+    end
+  end
+end
+
+describe CompactCSV::Table do
+  describe '#each' do
+    let(:csv) { CompactCSV.read('./spec/data/sample.csv', headers: true) }
+
+    it 'returns rows of iterator' do
+      csv.each do |row|
+        expect(row).to be_instance_of(CompactCSV::Row)
+      end
+    end
   end
 end

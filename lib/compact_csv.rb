5884aa25d2e9f3ef2c1165602c13d8a18dc7bb5c
NAKANO Ryusuke <rsk.nakano@gmail.com>
2015-12-20 19:27:31 +0900

    increase compatibility with CSV module

--- lib/compact_csv.rb
+++ lib/compact_csv.rb
@@ -3,11 +3,15 @@ require 'csv'
 class CompactCSV < CSV
   require_relative 'compact_csv/version'
 
+  class CompatibilityError < StandardError
+  end
+
   class Table < CSV::Table
   end
 
   class Row < CSV::Row
     def initialize(headers, fields, header_row = false)
+      headers.freeze
       @headers = headers
       @row_values = fields
     end
@@ -17,7 +21,11 @@ class CompactCSV < CSV
     end
 
     def row
-      headers.zip(@row_values).to_a
+      if @headers.size >= @row_values.size
+        @headers.zip(@row_values).to_a
+      else
+        @row_values.zip(@headers).map { |pair| pair.reverse! }
+      end
     end
 
     def header_row?
@@ -31,7 +39,7 @@ class CompactCSV < CSV
 
     def fields(*headers_and_or_indices)
       if headers_and_or_indices.empty?
-        size_diff = headers.size - @row_values.size
+        size_diff = @headers.size - @row_values.size
         if size_diff <= 0
           @row_values
         elsif size_diff > 0
@@ -45,7 +53,32 @@ class CompactCSV < CSV
     end
 
     def size
-      @row_values.size
+      [ @row_values.size, @headers.size ].max
+    end
+    alias_method :length, :size
+
+    def empty?
+      @row_values.empty?
+    end
+
+    def fetch(header, *varargs)
+      raise ArgumentError, "Too many arguments" if varargs.length > 1
+      pair = row.assoc(header)
+      if pair
+        pair.last
+      else
+        if block_given?
+          yield header
+        elsif varargs.empty?
+          raise KeyError, "key not found: #{header}"
+        else
+          varargs.first
+        end
+      end
+    end
+
+    def has_key?(header)
+      !!row.assoc(header)
     end
 
     def [](index)
@@ -73,6 +106,28 @@ class CompactCSV < CSV
         hash[row] = value
       end
       hash
+    end
+
+    def ==(other)
+      return row == other.row if other.is_a? CompactCSV::Row
+      row == other
+    end
+
+    # not compatible methods
+    def <<(arg)
+      raise CompatibilityError.new("CompactCSV does not allow to append fields into row. Please use CSV module.")
+    end
+
+    def push(*args)
+      raise CompatibilityError.new("CompactCSV does not allow to append fields into row. Please use CSV module.")
+    end
+
+    def delete(header_or_index, minimum_index = 0)
+      raise CompatibilityError.new("CompactCSV does not allow to delete fields from row. Please use CSV module.")
+    end
+
+    def delete_if(&block)
+      raise CompatibilityError.new("CompactCSV does not allow to delete fields from row. Please use CSV module.")
     end
   end
 
